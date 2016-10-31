19bad3292a1d21a68f3a653a5bdf0446fa0636ce
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-31 14:29:02 +0530

    Initial commit
    SEED™: Build System — KnotDB®.
    A Sequømics Product — http://sequomics.com/.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,26 @@
 #!/usr/bin/env node
+
+
+/*!
+ * KnotDB®
+ * ____________________________________________________________________
+ * Grunt, http://gruntjs.com/
+ * The JavaScript Task Runner.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 31-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Compass
+ * @build     : SEED™ — Katowice
+ *              |---- A Sequømics Product — http://sequomics.com/.
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "KnotDB®".
+ * ____________________________________________________________________
+ */
