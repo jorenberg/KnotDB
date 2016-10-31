075c932acb9570239216de49c4dba75a3c7a2893
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-31 22:01:36 +0530

    Initial commit
    SEED™: Build Versioning System — KnotDB®.
    A Sequømics Product — http://sequomics.com/.

---
+++ version.js
@@ -0,0 +1,14 @@
+/*!
+ * KnotDB®
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 31-Oct-2016
+ * @license   : Apache, version 2.0
+ * @build     : SEED™ — Katowice
+ *              |---- A Sequømics Product — http://sequomics.com/.
+ * ____________________________________________________________________
+ */
