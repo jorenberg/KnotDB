3f73ded1172a1e781bc14059b7df34a83b865a0f
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-31 13:38:47 +0530

    Initial commit
    run.sh, shell script to --/run/-- whole "KnotDB®" on {http://localhost:9001}.
    Usage: $ sh run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+# =========================== I'm Prabhat® ===========================
+#                               KnotDB®
+#       Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2008 - 2016, Prabhat Kumar, All rights reserved.
+# ====================================================================
