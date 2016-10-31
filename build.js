e258577627f8c6dce952ac67682352cd63ccacc7
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-31 20:16:30 +0530

    Initial commit
    The Heart of CoffeeScript --/Build System/-- of "KnotDB®".
    Help: An AMD loader plugin for CoffeeScript — https://github.com/requirejs/require-cs

---
+++ build.js
@@ -0,0 +1,20 @@
+/*!
+ * KnotDB®
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 31-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : CoffeeScript
+ * @build     : SEED™ — Katowice
+ *              |---- A Sequømics Product — http://sequomics.com/.
+ * ____________________________________________________________________
+ *
+ * --/The Heart of CoffeeScript Build System/-- of "KnotDB®".
+ * ____________________________________________________________________
+ */
