784107285d17bbf87f6b92b58912400903066bef
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-01 01:34:40 +0530

    Initial commit
    Index module of KnotXpert® of KnotDB®.
    A data visualisation engine for proteins knots database.

---
+++ core/source/engine/index.js
@@ -0,0 +1,16 @@
+/*!
+ * KnotXpert® of KnotDB®
+ * ---------------------
+ * A data visualisation engine for proteins knots database.
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * ___________________________________________________________________________
+ *
+ * @version   : 1.0.0
+ * @license   : MIT License
+ * @require   : Modernizr
+ * @require   : d3.js®
+ * @build     : SEED™ — Katowice
+ *              |---- A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ */
