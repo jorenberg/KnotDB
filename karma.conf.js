aad7e1463ed961146f1a73d2803e6a6247fec656
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-31 20:09:02 +0530

    Initial commit
    SEED™: Build + Test System — KnotDB®.
    A Sequømics Product — http://sequomics.com/.

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,26 @@
 #!/usr/bin/env node
+
+
+/*!
+ * KnotDB®
+ * ____________________________________________________________________
+ * karma, https://github.com/karma-runner/karma
+ * The Spectacular Test Runner for JavaScript.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 31-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * @build     : SEED™ — Katowice
+ *              |---- A Sequømics Product — http://sequomics.com/.
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "KnotDB®".
+ * ____________________________________________________________________
+ */
