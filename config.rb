cd2594141da87e509166b179d0fe6b676c69eed1
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-31 14:09:11 +0530

    Initial commit
    Usage: $ compass init

--- config.rb
+++ config.rb
@@ -1,1 +1,21 @@
 #!/usr/bin/env ruby
+
+
+# KnotDB®
+# -----------------------------------------
+# From the Desk of Sequømics Corporation.
+#     With ♥ — by Prabhat Kumar.
+# -----------------------------------------
+# @author     : Prabhat Kumar.
+# @copyright  : Sequømics Corporation.
+# @homepage   : http://sequomics.com/.
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Katowice
+# @date       : 31-Oct-2016
+# @license    : Apache, version 2.0
+# -----------------------------------------
+# Principle   : Standard Module Definition
+# -----------------------------------------
