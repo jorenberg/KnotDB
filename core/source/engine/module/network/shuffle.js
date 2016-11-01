d53f711b6152d4cd988204fceec4d8886dfb5aba
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-01 18:41:46 +0530

    Initial commit
    network/shuffle — module of KnotDB®.
    Fisher–Yates Shuffle/Random Shuffle/Naïve Swap/sort (random order) to visualizes sequence/properties co-occurrences.

---
+++ core/source/engine/module/network/shuffle.js
@@ -0,0 +1,10 @@
+/*!
+ * @file — shuffle
+ * @purpose: Fisher–Yates Shuffle/Random Shuffle/Naïve Swap/sort (random order) to visualizes sequence/properties co-occurrences.
+ * @module — network
+ * @private
+ * @version — 1.0.0
+ * @require — d3.js®
+ * @author — Prabhat Kumar <prabhat.genome@gmail.com> (http://prabhatkumar.org/)
+ * @license — Apache License
+ */
