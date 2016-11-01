bbf34a181ab119e0bd60a4c7618156487ec6a910
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-01 18:34:42 +0530

    Initial commit
    network/cooccurrence — module of KnotDB®.
    This matrix diagram visualizes sequence/properties co-occurrences in Victor Hugo’s Les Misérables.

---
+++ core/source/engine/module/network/cooccurrence.js
@@ -0,0 +1,10 @@
+/*!
+ * @file — cooccurrence
+ * @purpose: This matrix diagram visualizes sequence/properties co-occurrences in Victor Hugo’s Les Misérables.
+ * @module — network
+ * @private
+ * @version — 1.0.0
+ * @require — d3.js®
+ * @author — Prabhat Kumar <prabhat.genome@gmail.com> (http://prabhatkumar.org/)
+ * @license — Apache License
+ */
