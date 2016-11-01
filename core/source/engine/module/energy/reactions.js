51e9e3b231a02eb3ea91e7018fb2eac52cf3791d
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-01 16:54:17 +0530

    Initial commit
    energy/reactions — module of KnotDB®.

---
+++ core/source/engine/module/energy/reactions.js
@@ -0,0 +1,9 @@
+/*!
+ * @file — reactions
+ * @module — energy
+ * @private
+ * @version — 1.0.0
+ * @require — d3.js®
+ * @author — Prabhat Kumar <prabhat.genome@gmail.com> (http://prabhatkumar.org/)
+ * @license — Apache License
+ */
