232a115674e7a4545c39cabad7ee7ea447485134
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-02 21:04:29 +0530

    Initial commit
    A Grunt task to generate --/Protein(s) Data/-- in to JSON for "KnotDB®".
    SEED™ — A Sequømics Product — http://sequomics.com/.

---
+++ pub/data/data-generator.js
@@ -0,0 +1,18 @@
+/*!
+ * KnotDB®
+ * A manually curated dedicated computational proteomics proteins knots database.
+ * ______________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ______________________________________________________________________________
+ * @date      : 02-Nov-2016
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * @license   : Apache, version 2.0
+ * @build     : SEED™ — Katowice
+ *              |---- A Sequømics Product — http://sequomics.com/.
+ * ______________________________________________________________________________
+ */
